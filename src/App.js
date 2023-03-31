dbb0bae2170ac557fc4f139f4463c11679276e7e
Mads Hylleberg <madshyllebergn@gmail.com>
2023-03-31 11:23:47 +0200

    Bug fix.

--- src/App.js
+++ src/App.js
@@ -5,48 +5,55 @@ import Pagination from "./components/Pagination";
 
 export default function App()
 {
-    // * The list of Pokémon
+    // # The list of Pokémon
     const [pokemon, setPokemon] = useState();
-    // * Used for keeping track on which "page" of Pokémon, you are on
+    // # Used for keeping track on which "page" of Pokémon, you are on
     const [currentPageUrl,setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon?limit=20")
-    // * Used for keeping track on the next 20 Pokémon
+    // # Used for keeping track on the next 20 Pokémon
     const [nextPageUrl,setNextPageUrl] = useState();
-    // * Used to keep track on the prev. 20 Pokémon
+    // # Used to keep track on the prev. 20 Pokémon
     const [prevPageUrl,setPrevPageUrl] = useState();
+    // ! LOADING STATE
+    const [loading,setLoadingState] = useState(true);
 
-    // Get the Pokémon names from the API.
+    // * Get the Pokémon names from the API.
     async function fetchPokemon() {
         try {
+            setLoadingState(true);
             const response = await fetch(currentPageUrl);
             if (!response.ok) {
                 throw new Error("Response was NOT OK!");
             }
             const data = await response.json();
-            setNextPageUrl(data.next); // * Take the "next" value of the request
-            setPrevPageUrl(data.previous); // * Take the "previous" value of the request
-            setPokemon(data.results.map((p) => p.name)); // * Update the list of Pokémon!
+            setNextPageUrl(data.next); // # Take the "next" value of the request
+            setPrevPageUrl(data.previous); // # Take the "previous" value of the request
+            setPokemon(data.results.map((p) => p.name)); // ¤ Update the list of Pokémon!
+            setLoadingState(false);
         } catch (error) {
             console.log("Something went wrong")
         }
     }
 
-    // Dynamic update the list of shown Pokémon upon change.
+    // ¤ Dynamic update the list of shown Pokémon upon change.
     useEffect(() =>
     {
         fetchPokemon()
     }, [currentPageUrl]);
 
-    // Go to the next page of Pokémon.
+    // * Go to the next page of Pokémon.
     function goToNextPage()
     {
         setCurrentPageUrl(nextPageUrl);
     }
 
-    // Go to the prev. page of Pokémon.
+    // * Go to the prev. page of Pokémon.
     function goToPrevPage()
     {
         setCurrentPageUrl(prevPageUrl);
     }
+
+    if(loading) return "Loading...";
+
 
     return (
         <>
